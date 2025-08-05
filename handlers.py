a3153d2f0b6650401a5eb9165cc798141a31e07b
Behruzbek0771 <behruzbekqalandarov771@gmail.com>
2025-08-06 01:15:31 +0500

    toliq bot

--- handlers.py
+++ handlers.py
@@ -1,21 +1,92 @@
 from telegram import Update
 from telegram.ext import CallbackContext
 
+def handle_photo(update: Update, context: CallbackContext):
+    photo = update.message.photo[-1]
+    update.message.reply_photo(
+        photo=photo,
+        caption="Siz yuborgan rasm"
+    )
+
+def handle_video(update: Update, context: CallbackContext):
+    video = update.message.video
+    update.message.reply_video(
+        video=video,
+        caption="Siz yuborgan video"
+    )
+
+def handle_audio(update: Update, context: CallbackContext):
+    audio = update.message.audio
+    update.message.reply_audio(
+        audio=audio,
+        caption="Siz yuborgan audio"
+    )
+
+def handle_voice(update: Update, context: CallbackContext):
+    voice = update.message.voice
+    update.message.reply_voice(
+        voice=voice,
+        caption="Siz yuborgan ovozli xabar"
+    )
+
+def handle_video_note(update: Update, context: CallbackContext):
+    video_note = update.message.video_note
+    update.message.reply_video_note(
+        video_note=video_note
+    )
+
+def handle_document(update: Update, context: CallbackContext):
+    document = update.message.document
+    update.message.reply_document(
+        document=document,
+        caption="Siz yuborgan hujjat"
+    )
+
+def handle_location(update: Update, context: CallbackContext):
+    location = update.message.location
+    update.message.reply_location(
+        latitude=location.latitude,
+        longitude=location.longitude
+    )
+
+def handle_contact(update: Update, context: CallbackContext):
+    contact = update.message.contact
+    update.message.reply_contact(
+        first_name=contact.first_name,
+        phone_number=contact.phone_number
+    )
+
+def handle_dice(update: Update, context: CallbackContext):
+    dice = update.message.dice
+    update.message.reply_dice(
+        emoji=dice.emoji
+    )
+
+def handle_animation(update: Update, context: CallbackContext):
+    animation = update.message.animation
+    update.message.reply_animation(
+        animation=animation,
+        caption="Siz yuborgan gif"
+    )
+
+def handle_sticker(update: Update, context: CallbackContext):
+    sticker = update.message.sticker
+    update.message.reply_sticker(
+        sticker=sticker.file_id
+    )
 
 def start(update: Update, context: CallbackContext):
     user = update.message.from_user
-
     update.message.reply_text(
-        f'Assalomu alaykum! Botimizga xush kelibsiz {user.full_name}.\n\n'
-        'bu bot testing uchun'
+        f"Assalomu alaykum {user.full_name}! Botimizga xush kelibsiz.\n"
+        "Siz nima yuborsangiz, men shuni qaytaraman "
     )
-
 
 def help(update: Update, context: CallbackContext):
     update.message.reply_text(
-        'Qanday Yordam Kerak'
+        "Menga rasm, video, audio, kontakt, joylashuv, hujjat, gif, stiker, video xabar va boshqa fayllarni yuboring — men ularni sizga qaytaraman."
     )
-
 
 def echo(update: Update, context: CallbackContext):
     update.message.reply_text(update.message.text)
+
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 from config import TOKEN
 
-from handlers import start, help, echo
-
+from handlers import (start, help, echo , handle_photo, handle_contact,
+                    handle_video, handle_dice, handle_audio,handle_voice,
+                    handle_animation, handle_document, handle_location,
+                    handle_sticker, handle_video_note)
 
 def main() -> None:
     updater = Updater(TOKEN)
@@ -14,7 +16,18 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler('help', help))
 
     # message handler
-    dispatcher.add_handler(MessageHandler(Filters.text, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.contact, handle_contact))
+    dispatcher.add_handler(MessageHandler(Filters.photo, handle_photo))
+    dispatcher.add_handler(MessageHandler(Filters.video, handle_video))
+    dispatcher.add_handler(MessageHandler(Filters.dice, handle_dice))
+    dispatcher.add_handler(MessageHandler(Filters.audio, handle_audio))
+    dispatcher.add_handler(MessageHandler(Filters.voice, handle_voice))
+    dispatcher.add_handler(MessageHandler(Filters.document, handle_document))
+    dispatcher.add_handler(MessageHandler(Filters.location, handle_location))
+    dispatcher.add_handler(MessageHandler(Filters.animation, handle_animation))
+    dispatcher.add_handler(MessageHandler(Filters.sticker, handle_sticker))
+    dispatcher.add_handler(MessageHandler(Filters.video_note, handle_video_note))
 
     updater.start_polling()
     updater.idle()
